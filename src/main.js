43ac88ca1fe599b3c8f095d45dd22252a741239f
kaikeda <kaikeda.work@gmail.com>
2021-05-05 17:10:49 +0900

    [#1] Chat.vue as component

--- src/main.js
+++ src/main.js
@@ -3,17 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
-
-
 import ActionCable from 'actioncable';
 
 const cable = ActionCable.createConsumer('ws:localhost:3000/cable');
 Vue.prototype.$cable = cable;
 
-import Chat from './Chat.vue';
 new Vue({
-  render: h => h(Chat),
+  render: h => h(App),
 }).$mount('#app')
