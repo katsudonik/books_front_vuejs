d09a794b6b909492277b8771d0248357de5f8cbe
kaikeda <kaikeda.work@gmail.com>
2021-05-05 23:02:12 +0900

    [#1] await

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in messages" :key="item.message">
-        <p>{{ item.content }}</p>
-      </li>  
-    </ul>
-    <input v-model="msgBox" placeholder="message here"  @keydown.enter="speak" />
-    <button v-if="messageChannel" @click="speak">送信</button>    
+    <div v-if="client == ''">
+      <h1>SignIn</h1>
+      <label for="email">email</label>
+      <input id="email" type="email" v-model="email" />
+      <label for="password">password</label>
+      <input id="password" type="password" v-model="password" />
+      <button @click="sign_in">SignIn</button>
+    </div>
+
+    <div v-if="client != ''">
+      <ul>
+        <li v-for="item in messages" :key="item.message">
+          <p>{{ item.content }}</p>
+        </li>  
+      </ul>
+      <input v-model="msgBox" placeholder="message here"  @keydown.enter="speak" />
+      <button v-if="messageChannel" @click="speak">送信</button>
+    </div>
   </div>
 </template>
 
@@ -14,19 +25,25 @@
 export default {
   data() {
     return {
+      email: "",
+      password: "",
+      client: "",
+
       msgBox: "",
       messages: [],
       messageChannel: null,
     };
   },
   created() {
-    this.messageChannel = this.$cable.subscriptions.create( "MessageChannel",{
-      received: (data) => {
-        this.messages.push(data)
-      },
-    })
-    this.sign_in();
-    this.fetchMessages();
+    this.client = localStorage.getItem("client");
+    if (this.client != "") {
+      this.fetchMessages();
+      this.messageChannel = this.$cable.subscriptions.create( "MessageChannel",{
+        received: (data) => {
+          this.messages.push(data)
+        },
+      });
+    }
   },
   methods: {
     speak() {
@@ -35,17 +52,16 @@ export default {
       });
       this.msgBox = '';
     },
-    sign_in() {
-      // TODO sign in at login display & save response headers in local storage
-      this.axios({
+    async sign_in() {
+      await this.axios({
         method: 'post',
         url: 'http://localhost:3000/auth/sign_in', // TODO domain
         headers: {
           'Content-Type': 'application/json',
         },
         data: {
-          email: "example@example.com", // TODO from input
-          password: "password" // TODO from input
+          email: this.email,
+          password: this.password
         },
         timeout: 15000
       }).then((response) => {
@@ -53,12 +69,23 @@ export default {
         localStorage.setItem("client", response.headers["client"]);
         localStorage.setItem("uid", response.headers["uid"]);
         localStorage.setItem("access-token", response.headers["access-token"]);
+        this.client = response.headers["client"];
+        this.fetchMessages();
+        this.messageChannel = this.$cable.subscriptions.create( "MessageChannel",{
+          received: (data) => {
+            this.messages.push(data)
+          },
+        });
       }).catch((e) => {
-        alert(e);
+        console.log(e);
+        localStorage.removeItem("client");
+        localStorage.removeItem("uid");
+        localStorage.removeItem("access-token");
+        this.client = '';
       });
     },
-    fetchMessages() {
-      this.axios({
+    async fetchMessages() {
+      await this.axios({
         method: 'get',
         url: 'http://localhost:3000/chat_messages', // TODO domain
         headers: {
@@ -72,9 +99,16 @@ export default {
         timeout: 15000
       }).then((response) => {
         console.log(response);
+        localStorage.setItem("client", response.headers["client"]);
+        localStorage.setItem("uid", response.headers["uid"]);
+        localStorage.setItem("access-token", response.headers["access-token"]);
         this.messages = response.data['chat_messages']
       }).catch((e) => {
-        alert(e);
+        console.log(e);
+        localStorage.removeItem("client");
+        localStorage.removeItem("uid");
+        localStorage.removeItem("access-token");
+        this.client = '';
       });
     },
   },
